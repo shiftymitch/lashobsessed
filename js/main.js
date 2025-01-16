5c8393efba3ad1ef46cb2fddac7378e1f1fa6a5a
Mitch Henderson <shiftymitch@gmail.com>
2025-01-15 19:44:24 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -84,4 +84,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     animate();
+
+    // Change header style on scroll
+    const header = document.querySelector('header');
+    
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
 });
